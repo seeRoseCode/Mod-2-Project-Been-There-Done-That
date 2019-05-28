d5c748fa2b4822fb179bb4032d5ca491105fab28
seerosecode <keshalakemusic@gmail.com>
2019-05-28 16:43:37 -0400

    user CRUD functionality complete

--- BTDT/db/migrate/20190526222650_create_users.rb
+++ BTDT/db/migrate/20190526222650_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.integer :age
-      t.string :country
+      t.string :country_of_origin
       t.string :profile_pic
       t.integer :points
       t.string :password_digest
