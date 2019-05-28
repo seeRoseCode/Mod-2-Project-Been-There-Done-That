e11538d8411b88147b285293353dec11ce29bd75
github username <dekadekadeka@gmail.com>
2019-05-28 10:30:02 -0400

    authentication stuff

--- BTDT/app/controllers/users_controller.rb
+++ BTDT/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to users_path(@user)
         else 
             render :new
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:name, :age, :country, :profile_pic)
+        params.require(:user).permit(:name, :age, :country, :profile_pic, :password, :password_confirmation)
     end
 
 end
