dbbbc839e01dedc2c488a2d11e4daa6e73aa8822
seerosecode <keshalakemusic@gmail.com>
2019-05-28 17:33:37 -0400

    created functionality for creating multiple visits/user at once

--- BTDT/app/controllers/visits_controller.rb
+++ BTDT/app/controllers/visits_controller.rb
@@ -12,14 +12,19 @@ class VisitsController < ApplicationController
   end
 
   def create
-    @visit = Visit.create(visit_params)
-    if @visit.save
-      redirect_to visit_path(@visit)
-    else
-      render :new
+
+    visit_params["country_id"].each do |c|
+      if c != ""
+        @visit = Visit.create(
+          country_id: c.to_i,
+          user_id: session[:user_id]
+        )
+      end
     end
+  redirect_to user_path(session[:user_id])
   end
 
+############################################
   private
   def visit_params
     params.require(:visit).permit( :user_id, :country_id => [])
