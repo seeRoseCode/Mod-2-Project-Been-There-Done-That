2e4016f030a72b484d2140022b035284426ec193
github username <dekadekadeka@gmail.com>
2019-05-28 14:34:34 -0400

    added languages to JSON file

--- BTDT/db/seeds.rb
+++ BTDT/db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
+require 'pry'
 
 flags_file = File.read("./flags.json")
 flags_hash = JSON.parse(flags_file)
+
+Pry.start
