51bc15f2d4d12faf90f38e5f2d8a66f0a9826d9a
seerosecode <keshalakemusic@gmail.com>
2019-05-28 10:47:19 -0400

    controllers for visits and countries complete

--- BTDT/app/controllers/users_controller.rb
+++ BTDT/app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to users_path(@user)
-        else 
+            redirect_to user_path(@user)
+        else
             render :new
         end
     end
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
         @users = User.all
     end
 
-    def show 
+    def show
         @user = User.find(params[:id])
     end
 
