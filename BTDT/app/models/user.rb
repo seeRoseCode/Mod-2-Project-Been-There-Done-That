6d1332d6314e72dd5d76d1c5f3ffd2342b882d61
Preston Elliott <preston.elliott24@gmail.com>
2019-05-28 10:18:37 -0400

    added user methods in model

--- BTDT/app/models/user.rb
+++ BTDT/app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
     has_many :countries, through :visits
     has_many :posts
     has_many :comments
+
+    def self.all
+        @@users = User.all
+    end
+
+    def most_points
+        @@users.max_by { |user| user.points }
+    end
+
+    def avg_visits_per_user
+        User.all.average(:visits)
+    end
+
+    def age_range
+    end
 end
