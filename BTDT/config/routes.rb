8dc2e0fed8a437981fc6e101a2744b00f93b63c7
seerosecode <keshalakemusic@gmail.com>
2019-05-28 11:40:17 -0400

    created routes

--- BTDT/config/routes.rb
+++ BTDT/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users, :comments, :posts
+  resources :countries, only: [:index, :show]
+  resources :visits, only: [:index, :new, :create, :show]
+  get '/home', to: 'user#home'
 end
