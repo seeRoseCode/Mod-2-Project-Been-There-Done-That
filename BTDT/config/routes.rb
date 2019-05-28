9e09c7b89aca3992be0c182c5ba9a8e4d7f04094
seerosecode <keshalakemusic@gmail.com>
2019-05-28 11:56:27 -0400

    home and comments routes tested and working

--- BTDT/config/routes.rb
+++ BTDT/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :comments, :posts
+  resources :posts
+  resources :comments
+  resources :users
   resources :countries, only: [:index, :show]
   resources :visits, only: [:index, :new, :create, :show]
-  get '/home', to: 'user#home'
+  get '/home', to: 'users#home'#WORKING
 end
