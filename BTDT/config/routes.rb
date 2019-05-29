3b345d5cc25dd4dc7dff9643b720b8c7a0803ba2
github username <dekadekadeka@gmail.com>
2019-05-29 10:18:20 -0400

    styling navbar

--- BTDT/config/routes.rb
+++ BTDT/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#home', as: 'home' #WORKING
-  get '/login', to: 'users#login'#WORKING
+  get '/login', to: 'users#login', as: 'login' #WORKING
   post '/login', to: 'sessions#create'
   resources :posts#WORKING
   resources :comments#WORKING
