428b16eacde689b07a7555559d0acba905d189ed
github username <dekadekadeka@gmail.com>
2019-05-29 09:33:43 -0400

    first styling

--- BTDT/config/routes.rb
+++ BTDT/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/home', to: 'users#home'#WORKING
+  root 'users#home', as: 'home' #WORKING
   get '/login', to: 'users#login'#WORKING
   post '/login', to: 'sessions#create'
   resources :posts#WORKING
